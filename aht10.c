8eed7ea8cc7460ea30ce6d29aa8b54460787c46f
jdhxyy <jdh821@163.com>
2021-11-03 14:17:24 +0800

    DTS20211103141501 Modify:zhouxin Desc:i2c导致的模块死机

--- aht10.c
+++ aht10.c
@@ -4,13 +4,18 @@
 
 #include "aht10.h"
 #include "bror.h"
+#include "lagan.h"
 
-#include "driver/i2c.h"
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+
+#define TAG "aht10"
 
 // 线程堆栈大小.单位:KByte
 #define THREAD_STACK_SIZE 4
 
-#define AHT10_CLK_SPEED 100000
+// 时钟操作间隔.单位:us
+#define CLK_INTERVAL 5
 
 // 命令字
 #define CMD_INIT 0xE0
@@ -19,54 +24,46 @@
 // 读取时等待间隔.单位:ms
 #define GET_WAIT_INTERVAL 100
 
-#define SlaveAddress 0x70
-
-/*!< I2C port number */
-#define IIC_CTRL_NUM I2C_NUM_0
-
-/*!< I2C master write */
-#define WRITE_BIT 0x00
-/*!< I2C master read */
-#define READ_BIT 0x01
-/*!< I2C master will check ack from slave*/
-#define ACK_CHECK_EN 0x1
-/*!< I2C master will not check ack from slave */
-#define ACK_CHECK_DIS 0x0
-/*!< I2C ack value */
-#define ACK_VAL 0x0
-/*!< I2C nack value */
-#define NACK_VAL 0x1
+// 从机地址
+#define SLAVE_ADDRESS 0x70
 
 static Aht10Value ahtValue;
+static int gPinSda;
+static int gPinScl;
 
-static void i2cMasterInit(int pinSda, int pinScl);
+static void i2cInit(void);
 static void aht10Thread(void* param);
-static esp_err_t i2cWriteBytes(uint8_t WriteAddr, uint8_t* dataWr, uint8_t length);
-static esp_err_t i2cReadBytes(uint8_t* dataRd, uint8_t length);
+static void i2cStart(void);
+static void i2cStop(void);
+static bool i2cWaitAck(void);
+static void i2cSendAck(void);
+static void i2cSendNoAck(void);
+static void i2cSendByte(uint8_t data);
+static uint8_t i2cReceiveByte(void);
+static bool readBytes(uint8_t* data, int dataLen);
+static bool writeBytes(uint8_t addr, uint8_t* data, int dataLen);
 
 // Aht10Load 模块载入
 void Aht10Load(int pinSda, int pinScl) {
-    i2cMasterInit(pinSda, pinScl);
+    gPinSda = pinSda;
+    gPinScl = pinScl;
+    i2cInit();
 
-    uint8_t buf[2] = {0x08, 0x00};
-    i2cWriteBytes(CMD_INIT, buf, 2);
-
-    BrorThreadCreate(aht10Thread, "aht10Thread", BROR_THREAD_PRIORITY_LOWEST, 
+    BrorThreadCreate(aht10Thread, TAG, BROR_THREAD_PRIORITY_LOWEST, 
         THREAD_STACK_SIZE * 1024);
 }
 
-static void i2cMasterInit(int pinSda, int pinScl) {
-    i2c_config_t conf = {
-        .mode = I2C_MODE_MASTER,
-        .sda_io_num = pinSda,
-        .scl_io_num = pinScl,
-        .sda_pullup_en = GPIO_PULLUP_ENABLE,
-        .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = AHT10_CLK_SPEED,
-    };
-    i2c_param_config(I2C_NUM_0, &conf);
+static void i2cInit(void) {
+    gpio_config_t ioConf;
+    ioConf.intr_type = GPIO_INTR_DISABLE;
+    ioConf.mode = GPIO_MODE_INPUT_OUTPUT_OD;
+    ioConf.pin_bit_mask = (1<<gPinScl) | (1<<gPinSda);
+    ioConf.pull_down_en = 0;
+    ioConf.pull_up_en = 1;
+    gpio_config(&ioConf);
 
-    i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
+    gpio_set_level(gPinScl, true);
+    gpio_set_level(gPinSda, true);
 }
 
 static void aht10Thread(void* param) {
@@ -79,10 +76,20 @@ static void aht10Thread(void* param) {
     while (1) {
         buf[0] = 0x33;
         buf[1] = 0x00;
-        i2cWriteBytes(CMD_GET, buf, 2);
+        if (writeBytes(CMD_GET, buf, 2) == false) {
+            LE(TAG, "write bytes error!");
+            BrorDelay(1);
+            continue;
+        }
+
         BrorDelayMS(GET_WAIT_INTERVAL);
 
-        i2cReadBytes(buf, 6);
+        if (readBytes(buf, 6) == false) {
+            LE(TAG, "read bytes error!");
+            BrorDelay(1);
+            continue;
+        }
+
         ack = buf[0];
 
         if ((ack & 0x40) == 0) {
@@ -99,37 +106,143 @@ static void aht10Thread(void* param) {
     BrorThreadDeleteMe();
 }
 
-static esp_err_t i2cWriteBytes(uint8_t WriteAddr, uint8_t* dataWr, uint8_t length) {
-    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-    i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, SlaveAddress | WRITE_BIT, ACK_CHECK_EN);
-    i2c_master_write_byte(cmd, WriteAddr, ACK_CHECK_EN);
-    for (int i = 0; i < length; i++){
-        i2c_master_write_byte(cmd, (intptr_t)dataWr + i, ACK_CHECK_EN);
-    }
-    i2c_master_stop(cmd);
-    esp_err_t ret = i2c_master_cmd_begin(IIC_CTRL_NUM, cmd, 1000 / portTICK_RATE_MS);
-    i2c_cmd_link_delete(cmd);
-    return ret;
-}
-
-static esp_err_t i2cReadBytes(uint8_t* dataRd, uint8_t length) {
-    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-    i2c_master_start(cmd);
-
-    i2c_master_write_byte(cmd, SlaveAddress | READ_BIT, ACK_CHECK_EN);
-    for (int i = 0;i < (length-1);i++){
-        i2c_master_read_byte(cmd, dataRd + i, ACK_VAL);
-    }
-    i2c_master_read_byte(cmd, dataRd + length - 1, NACK_VAL);
-
-    i2c_master_stop(cmd);
-    esp_err_t ret = i2c_master_cmd_begin(IIC_CTRL_NUM, cmd, 1000 / portTICK_RATE_MS);
-    i2c_cmd_link_delete(cmd);
-    return ret;
-}
-
 // Aht10Get 读取温湿度
 Aht10Value Aht10Get(void) {
     return ahtValue;
+}
+
+static void i2cStart(void) {
+    gpio_set_level(gPinSda, 1);
+    ets_delay_us(CLK_INTERVAL);
+    gpio_set_level(gPinScl, 1);
+    ets_delay_us(CLK_INTERVAL);
+    gpio_set_level(gPinSda, 0);
+    ets_delay_us(CLK_INTERVAL);
+    gpio_set_level(gPinScl, 0);
+    ets_delay_us(CLK_INTERVAL);
+}
+
+static void i2cStop(void) {
+    gpio_set_level(gPinScl, 0);
+    ets_delay_us(CLK_INTERVAL);
+    gpio_set_level(gPinSda, 0);
+    ets_delay_us(CLK_INTERVAL);
+    gpio_set_level(gPinScl, 1);
+    ets_delay_us(CLK_INTERVAL);
+    gpio_set_level(gPinSda, 1);
+    ets_delay_us(CLK_INTERVAL);
+}
+
+static bool i2cWaitAck(void) {
+    gpio_set_level(gPinSda, 1);
+    ets_delay_us(CLK_INTERVAL);
+    gpio_set_level(gPinScl, 1);
+    ets_delay_us(CLK_INTERVAL);
+    if (gpio_get_level(gPinSda)) {
+        i2cStop();
+        return false;
+    }
+    gpio_set_level(gPinScl, 0);
+    return true; 
+}
+
+static void i2cSendAck(void) {
+    gpio_set_level(gPinSda, 0);
+    ets_delay_us(CLK_INTERVAL);
+    gpio_set_level(gPinScl, 1);
+    ets_delay_us(CLK_INTERVAL);
+    gpio_set_level(gPinScl, 0);    
+}
+
+static void i2cSendNoAck(void) {
+    gpio_set_level(gPinSda, 1);
+    ets_delay_us(CLK_INTERVAL);
+    gpio_set_level(gPinScl, 1);
+    ets_delay_us(CLK_INTERVAL);
+    gpio_set_level(gPinScl, 0);    
+}
+
+static void i2cSendByte(uint8_t data) {
+    uint8_t i = 8;
+    while (i--) {
+        gpio_set_level(gPinScl, 0);
+        ets_delay_us(CLK_INTERVAL);
+        if (data & 0x80) {
+            gpio_set_level(gPinSda, 1);
+        } else {
+            gpio_set_level(gPinSda, 0);
+        }
+        
+        ets_delay_us(CLK_INTERVAL);
+        data <<= 1;
+        gpio_set_level(gPinScl, 1);
+        ets_delay_us(CLK_INTERVAL);
+        gpio_set_level(gPinScl, 0);
+        ets_delay_us(CLK_INTERVAL);
+    }    
+}
+
+static uint8_t i2cReceiveByte(void) {
+    uint8_t i = 8;
+    uint8_t data = 0;
+    
+    gpio_set_level(gPinSda, 1);
+    while (i--) {
+        data <<= 1;
+        gpio_set_level(gPinScl, 0);
+        ets_delay_us(CLK_INTERVAL);
+        gpio_set_level(gPinScl, 1);
+        ets_delay_us(CLK_INTERVAL);
+        if (gpio_get_level(gPinSda)) {
+            data |= 0x01;
+        }
+    }
+    gpio_set_level(gPinScl, 0);
+    
+    return data;
+}
+
+static bool readBytes(uint8_t* data, int dataLen) {
+    i2cStart();
+    i2cSendByte(SLAVE_ADDRESS | 0x1);
+    if (i2cWaitAck() == false) {
+        i2cStop();
+        return false;
+    }
+    
+    for (int i = 0; i < dataLen; i++) {
+        *data++ = i2cReceiveByte();
+        if (i < dataLen - 1) {
+            i2cSendAck();
+        }
+    }
+    i2cSendNoAck();
+    i2cStop();
+
+    return true;
+}
+
+static bool writeBytes(uint8_t addr, uint8_t* data, int dataLen) {
+    i2cStart();
+    i2cSendByte(SLAVE_ADDRESS);
+    if(i2cWaitAck() == false) {
+        i2cStop();
+        return false;
+    }
+    i2cSendByte(addr);
+    if(i2cWaitAck() == false) {
+        i2cStop();
+        return false;
+    }
+
+    for (int i = 0; i < dataLen; i++) {
+        i2cSendByte(*data);
+        if(i2cWaitAck() == false) {
+            i2cStop();
+            return false;
+        }
+    }
+    
+    i2cStop();
+    return true;
 }
